68b1f0a3c8554cf3232167f2c60b34c959c262e4
dima <dprofe@yandex.ru>
2025-08-03 23:44:54 +0300

    add custom hostname

--- send-message.py
+++ send-message.py
@@ -5,11 +5,12 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("id", help="You ID, given by tg bot")
 parser.add_argument("message", help="Message to send")
+parser.add_argument("--hostname", help="Host where application is hosted", default="dprofe.ddns.net")
 
 args = parser.parse_args()
 
 requests.post(
-    'https://dprofe.ddns.net/tg-notify/',
+    f'https://{args.hostname}/tg-notify/',
     {
         'id': args.id,
         'message': args.message
